58dd4040212b9897ab7a27e53e7e0dd4768e6a4c
IsaacVerm <isaacverm@gmail.com>
2025-03-24 20:49:38 +0100

    specify parameters to filter games (rated, with clocks and evals ,...)

--- indexinator-games.sh
+++ indexinator-games.sh
@@ -3,11 +3,17 @@
 # if this has to be updated you can use the code below to get the current timestamp in milliseconds and subtract x hours
 # SINCE=$(($(date +%s) * 1000 - 4 * 60 * 60 * 1000))
 # echo $SINCE
+#
+# the full API reference for this endpoint is available at https://lichess.org/api#tag/Games/operation/apiGamesUser
 
 curl -X GET \
   -H "Accept: application/x-ndjson" \
   --get \
   --data "since=1742830512000" \
   --data "rated=true" \
+  --data "perfType=blitz" \
+  --data "analysed=true" \
+  --data "clocks=true" \
+  --data "evals=true" \
   https://lichess.org/api/games/user/indexinator \
   > games.jsonl
