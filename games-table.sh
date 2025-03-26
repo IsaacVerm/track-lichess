81e01bb0539212438bf8c3cb7f695d63dcb242b2
IsaacVerm <isaacverm@gmail.com>
2025-03-26 19:57:17 +0100

    turn moves string into array of strings

--- games-table.sh
+++ games-table.sh
@@ -1,2 +1,8 @@
 cat games.json |
-    jq 'map({id, moves, clocks})'
+    jq 'map({
+        id,
+        moves: (.moves | split(" ")),
+        clocks,
+        evals: (.analysis | map(.eval))
+    })' \
+    > test.json
