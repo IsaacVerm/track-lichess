d5ba426751074cb8e959c343ac7d9cda0aa522da
IsaacVerm <isaacverm@gmail.com>
2025-03-26 21:48:46 +0100

    create objects on move level

--- games-table.sh
+++ games-table.sh
@@ -4,5 +4,5 @@ cat games.json |
         moves: (.moves | split(" ")),
         clocks,
         evals: (.analysis | map(.eval))
-    })' \
+    }) | map([.moves, .clocks, .evals] | transpose | map({move: .[0], clock: .[1], eval: .[2]}))' \
     > test.json
