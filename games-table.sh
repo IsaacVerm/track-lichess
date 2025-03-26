5b73e39df52ef0e104e55201ff98edeceff4ac3c
IsaacVerm <isaacverm@gmail.com>
2025-03-26 22:53:15 +0100

    add Black and White player to every move

--- games-table.sh
+++ games-table.sh
@@ -1,8 +1,20 @@
 cat games.json |
     jq 'map({
         id,
+        player_white: .players.white.user.id,
+        player_black: .players.black.user.id,
         moves: (.moves | split(" ")),
         clocks,
         evals: (.analysis | map(.eval))
-    }) | map(.id as $game_id | [.moves, .clocks, .evals] | transpose | map({game_id: $game_id, move: .[0], clock: .[1], eval: .[2]}))' \
+    }) | map(.id as $game_id |
+            .player_white as $player_white |
+            .player_black as $player_black |
+            [.moves, .clocks, .evals] |
+            transpose |
+            map({game_id: $game_id,
+                player_white: $player_white,
+                player_black: $player_black,
+                move: .[0], 
+                clock: .[1],
+                eval: .[2]}))' \
     > test.json
